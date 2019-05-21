3aca80e7d806dbf233e5fc0e688ed25dbd2391e2
Jackson Gardner <madjack6603@gmail.com>
2019-05-21 11:47:55 -0400

    Jackson's better commit

--- Average.java
+++ Average.java
@@ -1,6 +1,6 @@
 public class Average {
     public void main(String args) {
-        system.out.println("Average: " + (Integer.parseInt(args[0]) + Integer.parseInt(args[1]))/2);
+        System.out.println("Average: " + (Integer.parseInt(args[0]) + Integer.parseInt(args[1]))/2);
 	//Missing quotation marks around Average: to make it a string
 	//Capitalize S in System.out.print
 	//Add a semicolon after the line of code
